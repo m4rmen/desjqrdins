b22e2272a83876f3225a9cb05bea6c1340f6cc33
Alexandre <alexandremarmen@gmail.com>
2024-09-13 20:33:28 -0400

    frontend done

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,25 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import '../styles/Home.css';
 const Home = () => {
   return (
-    <div>
+    <div className='main-container'>
       <header>
-        <h1>Welcome to My Website</h1>
+        <div className='header-container'>
+          <span>
+            <Link to="/"><small>A</small>A +</Link>
+          </span>
+          <span>
+            <Link to="/">English</Link>
+          </span>
+          <span>
+            <Link to="/">Nous joindre</Link>
+          </span>
+          <span>
+            <Link to="/">Aide</Link>
+          </span>
+        </div>
       </header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">About Us</Link>
-          </li>
-          <li>
-            <Link to="/">Services</Link>
-          </li>
-          <li>
-            <Link to="/">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className='sub-header'>
+        <div className='sub-header-container'>
+          <img src='Desjardins-logos.png' alt='logos' ></img>
+        </div>
+      </div>  
+      <div className='main-content'>
+        <div className='container-auth'>
+
+            <div className='auth-login' >
+                <div className='login-container'>
+                  <h1>Se connecter</h1>
+                  <form>
+                      <label>Identifiant</label>
+                      <input id='identifiant' type='text'></input>
+                      <div className='checkbox-container'>
+                        <input type='checkbox'></input>
+                        <label>Mémoriser</label>
+                      </div>
+                      <label>Mot de passe</label>
+                      <input id='password' type='password' ></input>
+                      <span id='attention'> <strong>Attention: </strong>Respecter majuscules et minuscules  </span>
+                      <span id='forgot-psswd'>Mot de passe oublié?</span>
+                      <div className='button-container'>
+                        <button><strong>Valider</strong></button>
+                      </div>
+                  </form>
+                </div>
+                <div><img src='login-footer.png' alt='forgot-password'></img></div>
+            </div>
+            <img id='girl-img' src='phone-girl.png' alt='auth-image' ></img>
+        </div>
+      </div>
+
+      <footer>
+        <img src='footer-info.png' alt='footer'></img>
+      </footer>
     </div>
   );
 };
